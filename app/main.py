4db11e18f23c72307549b3111a72ed8f281044f8
Mark Loney <markloney@loney.me>
2026-02-17 15:30:03 +0000

    Solution v3

--- app/main.py
+++ app/main.py
@@ -3,17 +3,19 @@ import time
 from hashlib import sha256
 
 
-TARGET_HASHES = (
-    "b4061a4bcfe1a2cbf78286f3fab2fb578266d1bd16c414c650c5ac04dfc696e1",
-    "cf0b0cfc90d8b4be14e00114827494ed5522e9aa1c7e6960515b58626cad0b44",
-    "e34efeb4b9538a949655b788dcb517f4a82e997e9e95271ecd392ac073fe216d",
-    "c15f56a2a392c950524f499093b78266427d21291b7d7f9d94a09b4e41d65628",
-    "4cd1a028a60f85a1b94f918adb7fb528d7429111c52bb2aa2874ed054a5584dd",
-    "40900aa1d900bee58178ae4a738c6952cb7b3467ce9fde0c3efa30a3bde1b5e2",
-    "5e6bc66ee1d2af7eb3aad546e9c0f79ab4b4ffb04a1bc425a80e6a4b0f055c2e",
-    "1273682fa19625ccedbe2de2817ba54dbb7894b7cefb08578826efad492f51c9",
-    "7e8f0ada0a03cbee48a0883d549967647b3fca6efeb0a149242f19e4b68d53d6",
-    "e5f3ff26aa8075ce7513552a9af1882b4fbc2a47a3525000f6eb887ab9622207",
+TARGET_HASHES = set(
+    [
+        "b4061a4bcfe1a2cbf78286f3fab2fb578266d1bd16c414c650c5ac04dfc696e1",
+        "cf0b0cfc90d8b4be14e00114827494ed5522e9aa1c7e6960515b58626cad0b44",
+        "e34efeb4b9538a949655b788dcb517f4a82e997e9e95271ecd392ac073fe216d",
+        "c15f56a2a392c950524f499093b78266427d21291b7d7f9d94a09b4e41d65628",
+        "4cd1a028a60f85a1b94f918adb7fb528d7429111c52bb2aa2874ed054a5584dd",
+        "40900aa1d900bee58178ae4a738c6952cb7b3467ce9fde0c3efa30a3bde1b5e2",
+        "5e6bc66ee1d2af7eb3aad546e9c0f79ab4b4ffb04a1bc425a80e6a4b0f055c2e",
+        "1273682fa19625ccedbe2de2817ba54dbb7894b7cefb08578826efad492f51c9",
+        "7e8f0ada0a03cbee48a0883d549967647b3fca6efeb0a149242f19e4b68d53d6",
+        "e5f3ff26aa8075ce7513552a9af1882b4fbc2a47a3525000f6eb887ab9622207"
+    ]
 )
 
 TARGET_COUNT = len(TARGET_HASHES)
@@ -31,7 +33,7 @@ def brute_force_password_block(
         stop_event: mp.Event
 ) -> None:
 
-    check_every = 10_000
+    check_every = 500
 
     for i, num in enumerate(range(start, end), start=1):
         if i % check_every == 0 and stop_event.is_set():
@@ -77,7 +79,7 @@ def brute_force_password() -> None:
             task.join()
 
         print(f"Number of passwords found: {len(found_passwords)}")
-        for pwd in sorted(sorted(found_passwords.keys())):
+        for pwd in sorted(found_passwords.keys()):
             print(pwd)
 
 
